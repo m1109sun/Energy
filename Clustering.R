bcdf7276aafe682199026069b3bfc0ecc18ff962
misun kim <m1109sun@gmail.com>
2018-08-29 13:54:37 +0900

    test

--- Clustering.R
+++ Clustering.R
@@ -86,6 +86,22 @@ kma_2016 <- kma_2016 %>%
 colnames(kma_2016) <- c("date", "temp", "humi")
 kma_2016$date <- as.POSIXct(kma_2016$date)
 
+# kma_2016에서 비어 있는 시간 채우기
+# temp랑 humi는 총 8784개씩 있어야 하고, 둘 다 NA가 2184개씩 존재함
+
+kma_2016 <- kma_2016 %>%
+  arrange(date) %>%
+  tidyr::complete(date = seq.POSIXt(as.POSIXct("2016-01-01 00:00:00"), as.POSIXct("2016-12-31 23:00:00"), by = "60 min")) 
+
+# na.fill (linear imputation)
+
+library(zoo)
+z <- c(NA, 2, NA, 1, 4, 5, 2, NA, 4, NA, NA, 6)
+zoo::na.fill(z, "extend")
+
+# linear imputation으로 kma_2016 missing 채우기
+kma_2016$temp <- zoo::na.fill(kma_2016$temp, "extend")
+kma_2016$humi <- zoo::na.fill(kma_2016$humi, "extend")
 
 ## 15분 간격으로 값을 다 채우고 난 후, 평균을 냄
 
@@ -138,7 +154,7 @@ mean_energy_dcast <- reshape2::dcast(mean_energy, time ~ id, value.var = "usage"
 ar_order <- c()
 for(i in 2:259){
   ar_id <- forecast::auto.arima(mean_energy_dcast[,i], d = 0, D = 0, max.q = 0)
-  ar_order[[i-1]] <- forecast::arimaorder(ar_id)[1]
+  ar_order[[i - 1]] <- forecast::arimaorder(ar_id)[1]
 }
 
 ar_p <- c(ar_order)
@@ -151,9 +167,10 @@ vcovHAC
 methods(vcovHAC)
 getAnywhere("vcovHAC.default")
 
-# for문을 이용한 code
 
-y_matrix <- t(as.matrix(mean_energy_dcast[,-1])) # row : id, column : usage (dim : 258 * 8784)
+# for문을 이용한 code (수렴하는 것)
+
+y_matrix <- t(as.matrix(mean_energy_dcast[,-1])) # row : id, column : time (dim : 258 * 8784)
 x_matrix <- matrix(1, nrow(y_matrix), ncol(y_matrix))
 
 cov_func <- function(initial, x_matrix, y_matrix, ar_order){
@@ -165,7 +182,7 @@ cov_func <- function(initial, x_matrix, y_matrix, ar_order){
     ## ols
     lm_residual <- stats::residuals(lm_matrix)
     res_matrix <- as.data.frame(matrix(data = lm_residual, nrow = ncol(y_matrix), byrow = FALSE))
-    # row : residual, column : id (dim : 8784 * 258)
+    # row : time, column : id (dim : 8784 * 258)
     colnames(res_matrix) <- NULL
     # res_matrix의 column 이름이 V1, V2... 이러니까 그냥 없앰
     
@@ -195,6 +212,9 @@ cov_func <- function(initial, x_matrix, y_matrix, ar_order){
 }
 
 cov_func(diag(258), x_matrix, y_matrix, 5)
+
+
+
 
 # while문을 이용한 code (전, 후의 차이가 0.001보다 작을 때 까지 반복)
 
@@ -258,7 +278,7 @@ x_matrix <- matrix(1, nrow(y_matrix), ncol(y_matrix))
 
 cov_func <- function(initial, x_matrix, y_matrix, ar_order, epsilon = 10^-3) {
   error <- 100; n <- 1;
-  cov_mat <- diag(nrow(y_matrix))
+  cov_mat <- list()
   lm_coef <- list()
   ar_shift <- list()
   lm_matrix <- lm(c(y_matrix) ~ c(x_matrix))
@@ -284,8 +304,8 @@ cov_func <- function(initial, x_matrix, y_matrix, ar_order, epsilon = 10^-3) {
     # row : id, column : residual (dim : 258 * 8779)
     
     ## cov matrix
-    cov_mat <- (e_matrix %*% t(e_matrix)) / ncol(e_matrix)
-    initial <- cov_mat
+    cov_mat[[n]] <- (e_matrix %*% t(e_matrix)) / ncol(e_matrix)
+    initial <- cov_mat[[n]]
     print(initial[1:6, 1:6])
     
     ## chol %*% matrix
@@ -305,14 +325,201 @@ cov_func <- function(initial, x_matrix, y_matrix, ar_order, epsilon = 10^-3) {
     
     n <- n + 1
   }
-  
-  result <- list(mu = lm_coef[[n - 1]], sigma = cov_mat)
+  result <- list(mu = lm_coef[[n - 2]], sigma = cov_mat[[n - 2]])
   return(result)
 }
 
 result <- cov_func(diag(258), x_matrix, y_matrix, 5)
 result$mu
 result$sigma
+
+
+# for문을 이용한 code  (이상해서 다시 바꿔보는 것.. 결과값이 그지같음.. 예전꺼랑 비슷함 variable addition)
+
+y_matrix <- t(as.matrix(mean_energy_dcast[,-1])) # row : id, column : time (dim : 258 * 8784)
+x_matrix <- matrix(rep(kma_2016$temp, each = nrow(y_matrix)), nrow = nrow(y_matrix)) # dim : 258 * 8784
+
+cov_func <- function(initial, x_matrix, y_matrix, ar_order){
+  cov_mat <- diag(nrow(y_matrix))
+  ar_shift <- list()
+  lm_matrix <- lm(c(y_matrix) ~ c(x_matrix))
+  
+  for(l in 1:20){
+    ## ols
+    lm_residual <- stats::residuals(lm_matrix)
+    res_matrix <- as.data.frame(matrix(data = lm_residual, nrow = ncol(y_matrix), byrow = TRUE))
+    # row : time, column : id (dim : 8784 * 258)
+    colnames(res_matrix) <- NULL
+    # res_matrix의 column 이름이 V1, V2... 이러니까 그냥 없앰
+    
+    ## AR
+    for(i in 1:(ar_order + 1)) {
+      ar_shift[[i]] <- unlist(res_matrix[i:(nrow(res_matrix) - ar_order + i - 1),])
+      names(ar_shift[[i]]) <- NULL # length : 2264982
+    }
+    
+    ar_matrix <- data.frame(rlist::list.cbind(ar_shift)) # dim : 2264982 * 6
+    c_name <- colnames(ar_matrix) # X1, X2, X3, X4, X5, X6
+    form <- paste0(c_name[ar_order + 1], " ~ ", paste0(c_name[-(ar_order + 1)], collapse = " + "))
+    # X6 ~ X1 + X2 + X3 + X4 + X5
+    e <- stats::residuals(lm(as.formula(form), data = ar_matrix)) # length : 2264982
+    e_matrix <- matrix(e, nrow = ncol(res_matrix), byrow = TRUE)
+    # row : id, column : time (dim : 258 * 8779)
+    
+    ## cov matrix
+    cov_mat <- (e_matrix %*% t(e_matrix)) / ncol(e_matrix)
+    initial <- cov_mat
+    print(initial[1:6, 1:6])
+    
+    ## chol %*% matrix
+    half_chol <- t(solve(chol(initial)))
+    y_matrix_up <- half_chol %*% y_matrix
+    x_matrix_up <- half_chol %*% x_matrix
+    lm_matrix <- lm(c(y_matrix_up) ~ c(x_matrix_up))
+    lm_coef <- coef(lm(c(y_matrix_up) ~ c(x_matrix_up)))[1]
+  
+    cat(lm_coef, "\n")
+  }
+}
+
+cov_func(diag(258), x_matrix, y_matrix, 5)
+
+
+
+
+
+
+
+## for문에 변수 두개 추가
+
+# for문을 이용한 code (수렴하는 것)
+
+y_matrix <- t(as.matrix(mean_energy_dcast[,-1])) # row : id, column : time (dim : 258 * 8784)
+x_temp <- matrix(rep(kma_2016$temp, each = nrow(y_matrix)), nrow = nrow(y_matrix)) # dim : 258 * 8784
+x_humi <- matrix(rep(kma_2016$humi, each = nrow(y_matrix)), nrow = nrow(y_matrix)) # dim : 258 * 8784
+
+
+cov_func <- function(initial, x_temp, x_humi, y_matrix, ar_order) {
+  cov_mat <- diag(nrow(y_matrix))
+  ar_shift <- list()
+  lm_matrix <- lm(c(y_matrix) ~ c(x_temp) + c(x_humi))
+  
+  for(l in 1:13){
+    ## ols
+    lm_coef <- coef(lm_matrix)
+    cat(lm_coef, "\n")
+    lm_residual <- stats::residuals(lm_matrix)
+    res_matrix <- as.data.frame(matrix(data = lm_residual, nrow = ncol(y_matrix), byrow = FALSE))
+    # row : time, column : id (dim : 8784 * 258)
+    colnames(res_matrix) <- NULL
+    # res_matrix의 column 이름이 V1, V2... 이러니까 그냥 없앰
+    
+    ## AR
+    for(i in 1:(ar_order + 1)) {
+      ar_shift[[i]] <- unlist(res_matrix[i:(nrow(res_matrix) - ar_order + i - 1),]) # length : 2264982
+    }
+    ar_matrix <- data.frame(rlist::list.cbind(ar_shift)) # dim : 2264982 * 6
+    c_name <- colnames(ar_matrix) # X1, X2, X3, X4, X5, X6
+    form <- paste0(c_name[ar_order + 1], " ~ ", paste0(c_name[-(ar_order + 1)], collapse = " + "))
+    # X6 ~ X1 + X2 + X3 + X4 + X5
+    e <- stats::residuals(lm(as.formula(form), data = ar_matrix))
+    e_matrix <- matrix(e, ncol = (nrow(res_matrix) - ar_order), byrow = TRUE)
+    # row : id, column : residual (dim : 258 * 8779)
+    
+    ## cov matrix
+    cov_mat <- (e_matrix %*% t(e_matrix)) / ncol(e_matrix)
+    initial <- cov_mat
+    print(initial[1:6, 1:6])
+    
+    ## chol %*% matrix
+    half_chol <- t(solve(chol(initial)))
+    y_matrix_up <- half_chol %*% y_matrix
+    x_temp_up <- half_chol %*% x_temp
+    x_humi_up <- half_chol %*% x_humi
+    lm_matrix <- lm(c(y_matrix_up) ~ c(x_temp_up) + c(x_humi_up))
+  }
+}
+
+cov_func(diag(258), x_temp, x_humi, y_matrix, 5)
+
+
+######## 2018-08-23 수정
+
+# for문을 이용한 code  (이상해서 다시 바꿔보는 것.. 결과값이 그지같음.. 예전꺼랑 비슷함 variable addition)
+
+y_matrix <- t(as.matrix(mean_energy_dcast[,-1])) # row : id, column : time (dim : 258 * 8784)
+x_matrix <- matrix(1, nrow = nrow(y_matrix), ncol = ncol(y_matrix)) # dim : 258 * 8784
+
+cov_func <- function(initial, x_matrix, y_matrix, ar_order) {
+  cov_mat <- diag(nrow(y_matrix))
+  ar_shift <- list()
+  ## ols
+  lm_matrix <- lm(c(y_matrix) ~ c(x_matrix))
+  lm_residual <- stats::residuals(lm_matrix)
+  res_matrix <- as.data.frame(matrix(data = lm_residual, nrow = nrow(y_matrix), byrow = FALSE))
+  # row : id, column : time (dim : 258 * 8784)
+  colnames(res_matrix) <- NULL
+  # res_matrix의 column 이름이 V1, V2... 이러니까 그냥 없앰
+    
+  ## AR
+  for(i in 1:(ar_order + 1)) {
+    ar_shift[[i]] <- unlist(res_matrix[, i:(ncol(res_matrix) - ar_order + i - 1)])
+    names(ar_shift[[i]]) <- NULL # length : 2264982
+  }
+    
+  ar_matrix <- data.frame(rlist::list.cbind(ar_shift)) # dim : 2264982 * 6
+  c_name <- colnames(ar_matrix) # X1, X2, X3, X4, X5, X6
+  form <- paste0(c_name[ar_order + 1], " ~ ", paste0(c_name[-(ar_order + 1)], collapse = " + "))
+  # X6 ~ X1 + X2 + X3 + X4 + X5
+  e <- stats::residuals(lm(as.formula(form), data = ar_matrix)) # length : 2264982
+  e_matrix <- matrix(e, nrow = nrow(res_matrix), byrow = FALSE)
+  # row : id, column : time (dim : 258 * 8779)
+    
+  ## cov matrix
+  cov_mat <- (e_matrix %*% t(e_matrix)) / ncol(e_matrix)
+  initial <- cov_mat
+  print(initial[1:6, 1:6])
+    
+  ## Affinity matrix
+  # gaussian kernel
+  gaus_ker <- function(x1, x2, alpha = 1) {
+    - alpha * abs(as.matrix(x1 - x2))
+  }
+  # affinity matrix
+  make_simil <- function(my_data) {
+    N <- nrow(my_data)
+    simil <- diag(N)
+    for (i in 1:N) {
+      for (j in 1:N) {
+        simil[i, j] <- gaus_ker(my_data[i, ], my_data[j, ])
+      }
+    }
+    simil
+  }
+  
+simil_mat <- make_simil(cov_mat)
+  
+  
+  
+  
+  
+  
+  ## chol %*% matrix
+    half_chol <- t(solve(chol(initial)))
+    y_matrix_up <- half_chol %*% y_matrix
+    x_matrix_up <- half_chol %*% x_matrix
+    lm_matrix <- lm(c(y_matrix_up) ~ c(x_matrix_up))
+    lm_coef <- coef(lm(c(y_matrix_up) ~ c(x_matrix_up)))[1]
+    
+    cat(lm_coef, "\n")
+  }
+}
+
+cov_func(diag(258), x_matrix, y_matrix, 5)
+
+## similarity measure는 두 개가 비슷할수록 값이 크고, 안 비슷할수록 0에 가깝거나 음수값을 가진다. (위키피디아) ## 원데이터에서 인접행렬을 만들 때는 보통 아래와 같이 가우시안 커널을 많이 사용함. 문서 간 거리가 멀리 떨어져 있을수록(유사하지 않을수록) 그 가중치는 줄어든다. 
+## 내가 정리한 affinity matrix는 거리가 멀수록 값이 작고, 거리가 가까울수록 값이 크다.
+library(speccalt)
 
 
 
